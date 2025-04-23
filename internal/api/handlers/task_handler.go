deb05ed26c95a1c172d2752ee8e91c2bef097ccd
Rishabh Saboo <rishabhsaboo7@gmail.com>
2025-04-23 16:58:24 +0530

    intialise task handler

---
+++ internal/api/handlers/task_handler.go
@@ -0,0 +1,12 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/pvnptl/task-service/internal/models"
+	"github.com/pvnptl/task-service/internal/service"
+)
+
